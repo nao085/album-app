22a05658b921b7ecbb6bbe4f27b72037a44b65a3
nao085 <tkn.0805.a@icloud.com>
2020-01-19 21:36:36 +0900

    add-my-page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'albums/index'
   root "albums#index"
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
 end
