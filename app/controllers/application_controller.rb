98d089c97ffaa37f9ba3f637beb38620dfca0b7e
nao085 <tkn.0805.a@icloud.com>
2020-02-19 03:31:48 +0900

    add-avatar-params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :avatar_cache, :remove_avatar])
+  end
 end
