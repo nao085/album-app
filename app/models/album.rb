ea724d88d8e45c44c557ef8d52bff921e2dae839
nao085 <tkn.0805.a@icloud.com>
2020-04-19 18:42:22 +0900

    fix validate

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,7 @@ class Album < ApplicationRecord
   belongs_to :user
   has_many :album_images, dependent: :destroy
   accepts_nested_attributes_for :album_images, allow_destroy: true, reject_if: :all_blank
-  validates_associated :album_images
+  # validates_associated :album_images
+  validates :album_images, presence: true, length: { maximum: 10}
   validates :title, {presence: true, length: {maximum: 40}}
 end
--- app/models/album_image.rb
+++ app/models/album_image.rb
@@ -2,5 +2,5 @@ class AlbumImage < ApplicationRecord
   belongs_to :album, optional: true
 
   mount_uploader :image, ImageUploader
-  validates :image, presence: true
+  # validates :image, presence: true
 end
