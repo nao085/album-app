62f1a8420f9641f58f63047375d938e529deb734
nao085 <tkn.0805.a@icloud.com>
2020-04-27 21:36:54 +0900

    add-flash-message

---
+++ app/javascript/flash.js
@@ -0,0 +1,7 @@
+// flash 一定時間経過後フェードアウト
+$(function(){
+  setTimeout("$('.flashBox--notice').fadeOut('slow')", 3000)
+});
+$(function(){
+  setTimeout("$('.flashBox--alert').fadeOut('slow')", 3000)
+});
