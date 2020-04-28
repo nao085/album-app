098f78bf5b4cf5795e0b61e9be85821961bd6e93
nao085 <tkn.0805.a@icloud.com>
2020-04-28 17:51:17 +0900

    いいね機能 非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,20 @@
 class LikesController < ApplicationController
   def create
+    @album = Album.find(params[:album_id])
     @like = current_user.likes.create(album_id: params[:album_id])
-    if @like.save
-      redirect_to album_path(@like.album.id)
-      flash[:notice] = 'いいねしました'
-    else
-      flash[:alert] = 'いいねできませんでした'
-    end
+    @like.save
+    #   redirect_to album_path(@like.album.id)
+    #   flash[:notice] = 'いいねしました'
+    # else
+    #   flash[:alert] = 'いいねできませんでした'
+    # end
   end
   
 
   def destroy
+    @album = Album.find(params[:album_id])
     @like = Like.find_by(album_id: params[:album_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 end
